58ba2bbdf75db89c98b1312c40227d2263cf6581
Janos Kocs <janos.kocs@outlook.com>
2022-06-21 22:41:37 +0100

    Basic html app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css'
-
 function App() {
-  return (
-    <>
 
-    </>
+
+  return (
+    <div>
+      <h1>Hello Janos, check out your todo list below!</h1>
+      <ol>
+        <li>Food shopping <button>Mark as complete</button> <button>Delete</button></li>
+        <li>Project <button>Mark as complete</button> <button>Delete</button></li>
+        <li>React <button>Mark as complete</button> <button>Delete</button></li>
+      </ol>
+      <form>
+        <input type='text' value='Enter new todo'></input>
+        <button>Add todo</button>
+      </form>
+    </div>
   );
 }
 
